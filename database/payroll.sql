aa3db706221c8ecd4062a6c5de1161839be7f25c
Asik-Ifthaker-Hamim <asikifthakerhamim75@gmail.com>
2024-12-26 15:21:58 +0600

    First-Version

--- database/payroll.sql
+++ database/payroll.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2024 at 09:41 AM
+-- Generation Time: Dec 26, 2024 at 10:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
